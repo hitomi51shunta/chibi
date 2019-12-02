4c820e6d4bf149f88c078721c6d85e57ccee5b8a
SUGISAKI Hitomi <21716053@jwu.ac.jp>
2019-12-02 16:48:19 +0900

    my second commit

--- chibi.py
+++ chibi.py
@@ -1,3 +1,4 @@
+
 import pegpy
 #from pegpy.tpeg import ParseTree
 peg = pegpy.grammar('chibi.tpeg')
@@ -54,27 +55,20 @@ class Mod(Binary):
         return self.left.eval(env) % self.right.eval(env)
 class Var(Expr):
     __slots__ = ['name']
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
-    def eval(self,env: dict):
+    def eval(self, env: dict):
         if self.name in env:
             return env[self.name]
         raise NameError(self.name)
 class Assign(Expr):
-    __slots__ = ['name','e']
-    def __init__(self,name,e):
+    __slots__ = ['name', 'e']
+    def __init__(self, name, e):
         self.name = name
         self.e = Expr.new(e)
-    def eval(self,env):
+    def eval(self, env):
         env[self.name] = self.e.eval(env)
         return env[self.name]
-print('少しテスト')
-env = {}
-e = Assign('x',Val(1)) # x = 1
-print(e.eval(env)) #1
-e = Assign('x',Add(Var('x'),Val(2))) #x = x + 2
-print(e.eval(env)) #3
-print('テスト終わり')
 def conv(tree):
     if tree == 'Block':
         return conv(tree[0])
@@ -83,35 +77,41 @@ def conv(tree):
     if tree == 'Add':
         return Add(conv(tree[0]), conv(tree[1]))
     if tree == 'Sub':
-        return Add(conv(tree[0]), conv(tree[1]))
+        return Sub(conv(tree[0]), conv(tree[1]))
     if tree == 'Mul':
-        return Add(conv(tree[0]), conv(tree[1]))
+        return Mul(conv(tree[0]), conv(tree[1]))
     if tree == 'Div':
-        return Add(conv(tree[0]), conv(tree[1]))
+        return Div(conv(tree[0]), conv(tree[1]))
     if tree == 'Mod':
-        return Add(conv(tree[0]), conv(tree[1]))
-    print('@TODO', tree.tag)
+        return Mod(conv(tree[0]), conv(tree[1]))
+    if tree == 'Var':
+        return Var(str(tree))
+    if tree == 'LetDecl':
+        return Assign(str(tree[0]), conv(tree[1]))
+    print('@TODO', tree.tag, repr(tree))
     return Val(str(tree))
-def run(src: str):
+def run(src: str, env: dict):
     tree = parser(src)
     if tree.isError():
         print(repr(tree))
     else:
         e = conv(tree)
-        print(repr(e))
-        print(e.eval({}))
+        print('env', env)
+        print(e.eval(env))
 def main():
     try:
+        env = {}
         while True:
             s = input('>>> ')
             if s == '':
                 break
-            run(s)
+            run(s, env)
     except EOFError:
         return
 if __name__ == '__main__':
     main()
-    
+
+
 
 
 
